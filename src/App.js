a057bf0a874b5ef756a70045f579c56aa0c648e5
Jocelin Nunes <jocelinnunes@gmail.com>
2022-04-21 11:29:31 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Nav_bar from './components/Nav_bar'
-import Main_routes from './components/Main_routes'
 import './App.css'
 
 export default () => {
   return (
-    <Router>
-      <Nav_bar />
-      <Main_routes />
-    </Router>
+    <>
+    k
+    </>
   )
 }
--- src/components/Contato.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  return (
-    <div>
-      <h1>Contato</h1>
-      <p>lorem</p>
-    </div>
-  )
-}
